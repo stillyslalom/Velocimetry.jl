b2dff19d0c28264a46f84469ab18f889a22dd9dc
Documenter.jl <documenter@juliadocs.github.io>
2020-12-28 22:10:27 +0000

    build based on 0568eca

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Velocimetry","category":"page"},{"location":"#Velocimetry","page":"Home","title":"Velocimetry","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Velocimetry]","category":"page"},{"location":"#Velocimetry.main-Tuple{}","page":"Home","title":"Velocimetry.main","text":"Docstring test\n\n\n\n\n\n","category":"method"}]
+}
