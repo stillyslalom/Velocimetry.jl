f65afe032eb6579d1188dff8a50a92a7f32f1ffc
Alex Ames <alexander.m.ames@gmail.com>
2021-08-23 14:11:18 -0500

    Fix Prana artifact folder

--- src/init.jl
+++ src/init.jl
@@ -1,7 +1,7 @@
 using Pkg.Artifacts
 
 function __init__()
-    prana_path = joinpath(artifact"Prana", "prana-0.1.0")
+    prana_path = joinpath(artifact"Prana", "prana-2.8.0.beta.r2021.01.19")
     if !isCI
         cd(prana_path) do
             mat"addpath($(prana_path))"
